8d24d174e37272aef61d338409ab08db146816af
KeepCodeing <3350680655@qq.com>
2021-08-26 20:38:25 +0800

    fetal:成员介绍完成

---
+++ src/views/index/components/MemberShower.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex items-center justify-center w-full my-5">
+    <div class="w-2/3">
+      <a-row justify="center">
+        <a-col
+          v-for="item in 5"
+          :key="item"
+          :span="4"
+          class="flex items-center justify-center h-32 mx-2"
+        >
+          <div
+            class="flex items-center justify-center font-bold text-white bg-blue-500 rounded-full  h-28 w-28"
+          >
+            介绍{{ item }}
+          </div>
+        </a-col>
+      </a-row>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+</style>
